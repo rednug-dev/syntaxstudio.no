aeee408d20ba34e39a3f723571b5d7897a7d7cae
Gunder Rollufson <gunder.rollufson@gmail.com>
2025-07-10 11:12:55 +0000

    vi må legge til dette hilsen google;
    <!-- Google tag (gtag.js) -->
    <scr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from 'next/font/google';
+import Script from 'next/script';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
 import { Analytics } from "@vercel/analytics/next"
@@ -34,6 +35,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="no" className="scroll-smooth dark">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17330083087"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-17330083087');
+          `}
+        </Script>
+      </head>
       <body className={cn(
         "font-body antialiased",
         inter.variable,
